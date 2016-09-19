f6ab4e6e0f4819f0a7fefe4b4946890f983515b5
d3jii <d3jii@ultroninc.com>
2016-09-19 14:29:05 +0100

    Fixed small issues

--- mox.js
+++ mox.js
@@ -3,7 +3,7 @@ module.exports = function (filepath){
 	var unzip   = require('unzip');
 	var fs      = require('fs'); 
  
-	fs.createReadStream(__dirname + '/localseeds/' + filepath).pipe(unzip.Extract( { path: './boomboom.zip' } ));
+	fs.createReadStream(__dirname + '/localseeds/' + filepath).pipe(unzip.Extract( { path: './' } ));
  
 
 }
